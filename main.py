2222a7d57c085bbbe2508e31f082382bf82df323
RobertLita <lita.robert@yahoo.com>
2022-03-22 21:25:52 +0200

    Etapa de selectie

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy as np
 import random
 import math
 
@@ -18,8 +17,7 @@ class Cromozom:
         self.codificare = self.codifica()
         self.valoare = self.transforma10()
         self.fitness = self.calculeazaFitness()
-        Cromozom.numarCromozomi += 1
-        self.id = Cromozom.numarCromozomi
+        self.pSelectie = 0
 
     def calculeazaFitness(self):
         return a * self.valoare * self.valoare + b * self.valoare + c
@@ -34,10 +32,13 @@ class Cromozom:
         return x
 
     def __repr__(self):
-        return f"{self.id}: {self.afisareCodificare()} x = {self.valoare} fitness = {self.fitness}"
+        return f"{self.afisareCodificare()} x = {self.valoare} fitness = {self.fitness}"
 
     def afisareCodificare(self):
         return "".join(str(gena) for gena in self.codificare)
+
+    def setPSelectie(self, F):
+        self.pSelectie = self.fitness / F
 
     @staticmethod
     def genereazaValoare():
@@ -69,14 +70,50 @@ def citire():
 
 class Populatie:
 
-    def __init__(self):
-        self.cromozomi = [Cromozom() for _ in range(dimPopulatie)]
+    def __init__(self, listaCromozomi=None):
+        self.cromozomi = listaCromozomi or [Cromozom() for _ in range(dimPopulatie)]
+        self.elitist = self.getElitist()
+        self.intervalSelectie = []
+        self.dupaSelectie = []
+        self.elemGenerateSelectie = []
 
     def __repr__(self):
-        return '\n'.join(str(cromozom) for cromozom in self.cromozomi)
+        return '\n'.join('   ' + str(i + 1) + ': ' + str(self.cromozomi[i]) for i in range(len(self.cromozomi)))
+
+    def getElitist(self):
+        mx = 0
+        e = 0
+        for cromozom in self.cromozomi:
+            if cromozom.fitness > mx:
+                mx = cromozom.fitness
+                e = cromozom
+        return e
+
+    def asociazaPSelectie(self):
+        s = 0
+        for cromozom in self.cromozomi:
+            s += cromozom.fitness
+        for cromozom in self.cromozomi:
+            cromozom.setPSelectie(s)
+
+    def vectorProbabilitati(self):
+        self.asociazaPSelectie()
+        v = [0]
+        sp = 0
+        for cromzom in self.cromozomi:
+            sp += cromzom.pSelectie
+            v.append(sp)
+        self.intervalSelectie = v
 
     def selectie(self):
-        pass
+        self.vectorProbabilitati()
+        selectati = []
+        for _ in range(dimPopulatie - 1):
+            elem = random.uniform(0, 1)
+            poz = Populatie.cautareBinara(self.intervalSelectie, elem) - 1
+            self.elemGenerateSelectie.append((elem, poz))
+            selectati.append(self.cromozomi[poz])
+        self.dupaSelectie = selectati
 
     def incrucisare(self):
         pass
@@ -87,7 +124,47 @@ class Populatie:
     def mutatie(self):
         pass
 
-if __name__ == '__main__':
+    def afisareDupaSelectie(self):
+        return '\n'.join('   ' + str(i + 1) + ': ' + str(self.dupaSelectie[i]) for i in range(len(self.dupaSelectie)))
+
+    def afisareIntervalSelectie(self):
+        return '\n'.join(
+            'C' + str(i + 1) + ': [' + str(self.intervalSelectie[i]) + ', ' + str(self.intervalSelectie[i + 1]) + ')'
+            for i in range(len(self.intervalSelectie) - 1))
+
+    def afisareElemGenerate(self):
+        return '\n'.join('S-a generat numarul ' + str(self.elemGenerateSelectie[i][0]) + ' si a fost selectat C' + str(
+            self.elemGenerateSelectie[i][1] + 1) for i in range(len(self.elemGenerateSelectie)))
+
+    def afisarePSelectie(self):
+        return '\n'.join('cromozom   ' + str(i + 1) + ' probabilitate ' + str(self.cromozomi[i].pSelectie) for i in
+                         range(len(self.cromozomi)))
+
+    @staticmethod
+    def cautareBinara(lista, k):
+        x = 0
+        y = len(lista) - 1
+        while x <= y:
+            mij = (x + y) // 2
+            if y - x == 1:
+                return y
+            if k < lista[mij]:
+                y = mij
+            else:
+                x = mij
+
+
+def rezolva():
     citire()
-    p = Populatie()
-    print(p)
+    print("Populatie initiala")
+    p = Populatie()                         # generam populatia initiala
+    print(p)                                # o afisam
+    p.selectie()                            # aplicam criteriul de selectie
+    print(p.afisarePSelectie())             # afisam probabilitatile de selectie
+    print(p.afisareIntervalSelectie())      # afisam intervalele de selectie
+    print(p.afisareElemGenerate())          # afisam elementele random generate de la "ruleta"
+    print(p.afisareDupaSelectie())          # afisam cromozomii care au fost selectati
+
+
+if __name__ == '__main__':
+    rezolva()
